9492eece04a0faf19f0df87a7d55898d3581fabe
Wallace Holanda <wallace.holanda@hotmail.com>
2022-05-15 22:03:45 -0300

    Criando o Carrossel de imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import Home from './components/pages/Home';
+import Home from './components/pages/Home/Home';
 import Login from './components/pages/Login'
 
 function App() {
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import Footer from "../../layout/Footer";
+import Navbar from "../../layout/Navbar";
+import Carrossel from "./Carrossel/Carrossel";
+
+function Home() {
+    return (
+        <div>
+            <Navbar/>
+            <Carrossel />
+
+            <h1>Cards</h1>
+
+            <h1>Cards</h1>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default Home;
