8203dac19f93452b5b661fa568de880c31807f25
Wallace Holanda <wallace.holanda@hotmail.com>
2022-05-25 00:23:10 -0300

    Adiantando a visualização do produto

--- src/components/pages/Produto/Produto.js
+++ src/components/pages/Produto/Produto.js
@@ -1,10 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Row, Column } from 'react-foundation';
-// import { Panel } from 'rsuite';
+import { Link } from 'react-router-dom';
+
+import ReactStars from "react-rating-stars-component";
+import SearchIcon from '@rsuite/icons/Search';
 
 import styles from './Produto.module.css';
-
 import Footer from "../../layout/Footer";
 import Navbar from "../../layout/Navbar";
 import CardProduto from '../Home/Produto/CardProduto';
@@ -13,18 +15,24 @@ import produtoImage from '../../../img/smartphone.png';
 function Produto() {
     return (
         <div>
+
+            <div className={styles.divbusca}>
+                <input className={styles.inputbusca} placeholder="O que você está procurando? "/>
+                <button className={styles.buttonbusca}><SearchIcon /></button>
+            </div>
+
             <Navbar />
-            <p> Smartphone Xiaomi Redmi Note 9s </p>
+            <h2 className={styles.nomeproduto}> Smartphone Xiaomi Redmi Note 9s </h2>
 
             <div className={styles.divcentral}>
-                <Row>
+                <Row className={styles.opcoesproduto}>
                     <a href="#"> Código: 00928477126 |</a>
                     <a href="#"> Descrição completa |</a>
                     <a href="#"> Xiaomi</a>
                 </Row>
 
                 <div className={styles.panel}>
-                    <Column className={styles.primeiracoluna}>
+                    <Column>
 
                         <div className={styles.galeria}>
                             <img className={styles.imgcelular} src={produtoImage} />
@@ -33,24 +41,40 @@ function Produto() {
                     </Column>
 
 
-                    <Column>
-                        <h1>Texto</h1>
-                        <p>Smartphone Xiaomi Redmi Note 9S 64GB Cinza 4G</p>
+                    <Column className={styles.segundaColuna}>
+                        <label className={styles.label}>Cor </label>
+                        <select className={styles.input}>
+                            <option value="azul">Azul</option>
+                            <option value="diamante">Diamante</option>
+                        </select>
 
-                        <h1>Texto</h1>
-                        <p>Smartphone Xiaomi Redmi Note 9S 64GB Cinza 4G</p>
+                        <label className={styles.label}>Armazenamento </label>
+                        <select className={styles.input}>
+                            <option value="128">128 GB</option>
+                            <option value="254">254 GB</option>
+                        </select>
 
-                        <h1>Texto</h1>
-                        <p>Smartphone Xiaomi Redmi Note 9S 64GB Cinza 4G</p>
+                        <p>Vendido e entregue por CrazyCell</p>
+                        <p>De: R$ 1700,00</p>
+                        <h4>Por: R$ 1699,00</h4>
 
-                        <h1>Texto</h1>
-                        <p>Smartphone Xiaomi Redmi Note 9S 64GB Cinza 4G</p>
+                        <div className={styles.estrelas}>
+                            <p className={styles.textoestrelas}>Avaliação </p>
+                            <ReactStars
+                                count={5}
+                                value={5}
+                                size={23}
+                                activeColor="#ffd700"
+                            />
+                        </div>
 
+                        <Link to="/cadastrocnpjp2" className={styles.button}>Continuar</Link>
 
                     </Column>
                 </div>
 
-                <p>
+                <h3 className={styles.produtoinfo}>Informações do Produto</h3>
+                <p className={styles.descricao}>
                     Smartphone Xiaomi Redmi Note 9S 64GB Cinza 4G+ - 4GB RAM Tela 6,67” Câm. Quádrupla + Selfie 16MP
                     Tenha um smartphone diferenciado em suas mãos! O Redmi Note 9S da Xiaomi produz fotos de nível
                     profissional, pois oferece um conjunto de 04 câmeras na traseira, permitindo que você capte as
@@ -61,7 +85,8 @@ function Produto() {
                     ou vídeos!
                 </p>
 
-                <CardProduto titulo = "Produtos Mais Vendidos" />  
+                <CardProduto titulo="Quem viu esse produto viu estes também" />
+                <CardProduto titulo="Produtos Similares" />
 
             </div>
 
